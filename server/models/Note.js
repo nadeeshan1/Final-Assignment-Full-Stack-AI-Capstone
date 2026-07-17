59005f03fad8fbb50593ddb9741e5a35dfca4b39
nadeeshan1 <kavindunadeeshan2002@gmail.com>
2026-07-17 10:43:40 +0530

    feat: add note mongoose model

---
+++ server/models/Note.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  subject: {
+    type: String,
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model('Note', noteSchema);
